37e1796dcacb7b809a1a3ac1ffa90dd7a01c2f04
Elad Hirsch <eladh@jfrog.com>
2019-01-20 19:02:33 +0200

    use podTemplate

--- client-app/Jenkinsfile.groovy
+++ client-app/Jenkinsfile.groovy
@@ -21,7 +21,6 @@ podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
         }
 
         stage ('Install npm') {
-            rtNpm.tool = 'nodejs-tool'
             rtNpm.install buildInfo: buildInfo, path: 'client-app'
         }
 
