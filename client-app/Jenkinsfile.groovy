74e1e653793905b8d78663a6671d555f17fa16d8
Elad Hirsch <eladh@jfrog.com>
2019-01-20 18:16:21 +0200

    setup github repo

--- client-app/Jenkinsfile.groovy
+++ client-app/Jenkinsfile.groovy
@@ -1,34 +1,41 @@
-node('generic') {
-    def server = Artifactory.server "artifactory"
-    def rtNpm = Artifactory.newNpmBuild()
-    def buildInfo
+podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
+        containerTemplate(name: 'node', image: 'node', command: 'cat', ttyEnabled: true)]) {
+
+    node('jenkins-pipeline') {
+
+        def server = Artifactory.server "artifactory"
+        def rtNpm = Artifactory.newNpmBuild()
+        def buildInfo
 
 
-    stage ('Clone') {
-        git url: 'https://github.com/eladh/demos.git' ,credentialsId: 'github'
+        stage ('Clone') {
+            git url: 'https://github.com/eladh/demos.git' ,credentialsId: 'github'
 
-    }
+        }
 
-    stage ('Artifactory configuration') {
-        rtNpm.deployer repo: 'npm-local', server: server
-        rtNpm.resolver repo: 'npm-remote', server: server
-        buildInfo = Artifactory.newBuildInfo()
-    }
+        stage ('Artifactory configuration') {
+            rtNpm.deployer repo: 'npm-local', server: server
+            rtNpm.resolver repo: 'npm-remote', server: server
+            buildInfo = Artifactory.newBuildInfo()
+        }
 
-    stage ('Install npm') {
-        rtNpm.install buildInfo: buildInfo, path: 'client-app'
-    }
+        stage ('Install npm') {
+            rtNpm.install buildInfo: buildInfo, path: 'client-app'
+        }
 
-    stage ('Build npm') {
-        sh 'npm run build --prefix client-app'
-        sh 'cp client-app/package.json client-app/dist/'
-    }
+        stage ('Build npm') {
+            container('kubectl') {
+                sh 'npm run build --prefix client-app'
+                sh 'cp client-app/package.json client-app/dist/'
+            }
+        }
 
-    stage ('Publish npm') {
-        rtNpm.publish buildInfo: buildInfo, path: 'client-app/dist'
-    }
+        stage ('Publish npm') {
+            rtNpm.publish buildInfo: buildInfo, path: 'client-app/dist'
+        }
 
-    stage ('Publish build info') {
-        server.publishBuildInfo buildInfo
+        stage ('Publish build info') {
+            server.publishBuildInfo buildInfo
+        }
     }
 }
