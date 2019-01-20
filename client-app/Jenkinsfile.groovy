65b75ab02000e98fa0f0813096f43bfce765c91a
Elad Hirsch <eladh@jfrog.com>
2019-01-20 18:27:19 +0200

    use podTemplate

--- client-app/Jenkinsfile.groovy
+++ client-app/Jenkinsfile.groovy
@@ -7,35 +7,36 @@ podTemplate(label: 'jenkins-pipeline' , cloud: 'k8s' , containers: [
         def rtNpm = Artifactory.newNpmBuild()
         def buildInfo
 
+        container('node') {
 
-        stage ('Clone') {
-            git url: 'https://github.com/eladh/demos.git' ,credentialsId: 'github'
+            stage('Clone') {
+                git url: 'https://github.com/eladh/demos.git', credentialsId: 'github'
 
-        }
+            }
 
-        stage ('Artifactory configuration') {
-            rtNpm.deployer repo: 'npm-local', server: server
-            rtNpm.resolver repo: 'npm-remote', server: server
-            buildInfo = Artifactory.newBuildInfo()
-        }
+            stage('Artifactory configuration') {
+                rtNpm.deployer repo: 'npm-local', server: server
+                rtNpm.resolver repo: 'npm-remote', server: server
+                buildInfo = Artifactory.newBuildInfo()
+            }
 
-        stage ('Install npm') {
-            rtNpm.install buildInfo: buildInfo, path: 'client-app'
-        }
+            stage('Install npm') {
+                rtNpm.install buildInfo: buildInfo, path: 'client-app'
+            }
 
-        stage ('Build npm') {
-            container('node') {
+            stage('Build npm') {
                 sh 'npm run build --prefix client-app'
                 sh 'cp client-app/package.json client-app/dist/'
             }
+
         }
 
-        stage ('Publish npm') {
-            rtNpm.publish buildInfo: buildInfo, path: 'client-app/dist'
-        }
-
-        stage ('Publish build info') {
-            server.publishBuildInfo buildInfo
-        }
+//        stage ('Publish npm') {
+//            rtNpm.publish buildInfo: buildInfo, path: 'client-app/dist'
+//        }
+//
+//        stage ('Publish build info') {
+//            server.publishBuildInfo buildInfo
+//        }
     }
 }
